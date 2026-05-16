fd330874bbe957572160895c99efe430f5278326
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2026-05-16 23:43:37 +0900

    Updated widgets view

---
+++ samples/TrayIconFlyout.Wasdk.Sample.App/ViewModels/WidgetStyleTrayIconFlyoutViewModel.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 0x5BFA. All rights reserved.
+// Licensed under the MIT license.
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace U5BFA.Libraries;
+
+internal partial class WidgetStyleTrayIconFlyoutViewModel : ObservableObject
+{
+    [ObservableProperty]
+    internal partial string? NowDateText { get; set; }
+
+    internal WidgetStyleTrayIconFlyoutViewModel()
+    {
+        NowDateText = "May 5";
+    }
+}
